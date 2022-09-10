c81e966059642412d1152bd6fcfdcaf46b414423
sheryylli <sherylli2015@gmail.com>
2022-09-10 08:12:56 -0400

    Update Features.jsx

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -4,27 +4,24 @@ import './features.css';
 
 const featuresData = [
   {
-    title: 'Improving end distrusts instantly',
-    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
+    title: 'Collaborate and Manage',
+    text: 'Share and create with colleagues. Comment, suggest, manage teams and delegate tasks.',
   },
   {
-    title: 'Become the tended active',
-    text: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.',
+    title: 'No Code Customization',
+    text: 'Write without worrying about having to debug. Customize the format, insert tables and graphs, equations, footnotes and more all without code.',
   },
   {
-    title: 'Message or am nothing',
-    text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.',
+    title: 'Conversion and Export',
+    text: 'Be able to see auto generate LaTeX code of your current document. Export and share, download and publish.',
   },
-  {
-    title: 'Really boy law county',
-    text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush..',
-  },
+ 
 ];
 
 const Features = () => (
   <div className="gpt3__features section__padding" id="features">
     <div className="gpt3__features-heading">
-      <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
+      <h1 className="gradient__text">With Polytex, write and publish papers faster than before with features that make the experience convenient, collaborative, and efficient.</h1>
       <p>Request Early Access to Get Started</p>
     </div>
     <div className="gpt3__features-container">
