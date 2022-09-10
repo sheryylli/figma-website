77a51a8caf6828e4d1ebca2825f6781a2d12ae1d
sheryylli <sherylli2015@gmail.com>
2022-09-10 08:35:47 -0400

    Update Footer.jsx

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -5,7 +5,7 @@ import './footer.css';
 const Footer = () => (
   <div className="gpt3__footer section__padding">
     <div className="gpt3__footer-heading">
-      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+      <h1 className="gradient__text">Join polytex!</h1>
     </div>
 
     <div className="gpt3__footer-btn">
@@ -14,8 +14,8 @@ const Footer = () => (
 
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
-        <img src={gpt3Logo} alt="gpt3_logo" />
-        <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+        //<img src={gpt3Logo} alt="gpt3_logo" />
+        <p>yay, <br /> All Rights Reserved</p>
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Links</h4>
@@ -32,15 +32,13 @@ const Footer = () => (
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
-        <p>085-132567</p>
-        <p>info@payme.net</p>
+        <p>hi</p>
+    
+    
       </div>
     </div>
 
-    <div className="gpt3__footer-copyright">
-      <p>@2021 GPT-3. All rights reserved.</p>
-    </div>
+    
   </div>
 );
 
