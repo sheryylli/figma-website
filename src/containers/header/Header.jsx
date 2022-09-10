d67b9ab31caa037b7b0447ecefcb9b1dae8c414d
sheryylli <sherylli2015@gmail.com>
2022-09-10 05:51:08 -0400

    Revert "Update Header.jsx"
    This reverts commit 996e761bb874fb7151280cb8a78db9175f63ffac.

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import people from '../../assets/people.png';
-import main from '../../assets/main.png';
+import ai from '../../assets/ai.png';
 import './header.css';
 
 const Header = () => (
