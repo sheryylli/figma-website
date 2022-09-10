02c4eeb5fc706a15d002a8613eb62b4234984d6e
sheryylli <sherylli2015@gmail.com>
2022-09-10 05:10:46 -0400

    Update Header.jsx

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,8 +6,8 @@ import './header.css';
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <h1 className="gradient__text">Collaborative design interface for academic writing</h1>
+      <p>Write and format professional papers- visually. Experience LaTeX features, integrate equations, and fully customizable elements with no code- seamlessly.</p>
 
       <div className="gpt3__header-content__input">
         <input type="email" placeholder="Your Email Address" />
