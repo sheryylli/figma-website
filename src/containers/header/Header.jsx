996e761bb874fb7151280cb8a78db9175f63ffac
sheryylli <sherylli2015@gmail.com>
2022-09-10 05:22:22 -0400

    Update Header.jsx

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import people from '../../assets/people.png';
-import ai from '../../assets/ai.png';
+import main from '../../assets/main.png';
 import './header.css';
 
 const Header = () => (
